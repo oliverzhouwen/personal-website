51e9e43eefbf5b573b8130310e1aea574305d129
Wen Zhou <oliver.zhou@msn.com>
2023-01-04 22:28:07 +1100

    Change construction component

--- src/components/Construction/construction.container.styles.tsx
+++ src/components/Construction/construction.container.styles.tsx
@@ -5,4 +5,8 @@ export const ConstructionContainer = styled.div`
     text-transform: uppercase;
     font-size: 3rem;
     font-weight: bold;
+    @media (max-width: 850px) {
+        font-size: 1.2rem;
+        font-weight: bold;
+    }
 `
